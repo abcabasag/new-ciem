d3b6d025607e05818c1ed6783ef0a2927a90ecb5
Ma. Roxette Rojas <102643882+prmxrjas@users.noreply.github.com>
2024-06-11 23:31:56 +0800

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -1,69 +1,114 @@
-from dash_iconify import DashIconify as di
-from dash import html
-from apps import commonmodule as cm
-from dash import dcc, html
-from dash.dependencies import Input, Output, State
-from dash.exceptions import PreventUpdate
-from datetime import datetime
+import dash
 import dash_bootstrap_components as dbc
+import hashlib
+from dash import callback_context, dcc, html
+from dash.dependencies import Input, Output, State
 from app import app
-layout=html.Div([
-        dbc.Card([
+from apps import dbconnect as db
+from dash.exceptions import PreventUpdate
+
+layout = html.Div(
+    [
+        dcc.Store(id='account_id_store', storage_type='session', data=0),  # Store user ID
+        html.Video(src="./assets/appsor.mp4", className="video", muted=True, width="100%", loop=True, autoPlay=True),
+        dbc.Card(
             dbc.CardBody(
                 [
-                html.Div([
-                    dbc.CardHeader(html.H1("LOGIN")),
-                        dbc.Input(id='uname', type='text', className='input', placeholder='Username'),
-                        dbc.Input(type='password', id='pword', className='input', placeholder='Password'),
-                        html.H4(id='errormessage', className='error'),
-                        dbc.Button('Log In', id='submit-val', className='loginbutton', n_clicks=0),
+                    dbc.CardHeader(html.H1("LOG IN")),
+                    html.Br(),
 
-                ],className='half left'),
-                html.Div([
-                     dbc.Carousel(
-                    items=[
-                        {"key": "1", "src": "/assets/A1.jpg"},
-                        {"key": "2", "src": "/assets/A2.jpg"},
-                        {"key": "3", "src": "/assets/A3.jpg"},
-                        {"key": "4", "src": "/assets/A4.jpg"},
-                        {"key": "5", "src": "/assets/A5.jpg"},
-                        {"key": "6", "src": "/assets/A6.jpg"},
-                        {"key": "7", "src": "/assets/A7.jpg"},
-                        {"key": "8", "src": "/assets/A8.jpg"},
-                        {"key": "9", "src": "/assets/A79.jpg"},
-                    ],
-                    controls=False,
-                    indicators=False,
-                    interval=1000,
-                    ride="carousel",
-                    style={'aspect-ratio':'16/9','height':'450px','overflow-y':'hidden'})
-                ],className='half')
+                    dbc.Alert(id='login_alert', is_open=False),
+                    dbc.Input(id='uname', type='text', className='input', placeholder='Username'),
+                    html.Br(),
+
+                    dbc.Input(type='password', id='pword', className='input', placeholder='Password'),
+                    html.Br(),
+
+                    dbc.Checklist(
+                        options=[{"label": "Show Password", "value": 1}],
+                        value=[],
+                        id="show_pword",
+                        inline=True,
+                    ),
+                    html.Br(),
+                    dbc.Row(
+                        dbc.Col(
+                            dbc.Button("Log in", color="primary", className="loginbutton", id='login_loginbtn'),
+                            width={'size': 4, 'offset': 8},
+                            className="d-flex justify-content-end"
+                        )
+                    ),
                 ],
+                className='half left'
             ),
-        ], class_name='flex small')
-    ],className='FullScreen')
+            class_name='flex small'
+        ),
+    ],
+    className='FullScreen'
+)
+
 
 @app.callback(
-    [Output('errormessage','children'),Output('login-auth','data')],
-    [Input('submit-val','n_clicks')],
-    [State('uname','value'),State('pword','value')]
+    [
+        Output('login_alert', 'color'),
+        Output('login_alert', 'children'),
+        Output('login_alert', 'is_open'),
+        Output('auth', 'data'),  # Update auth data upon successful login
+    ],
+    [
+        Input('login_loginbtn', 'n_clicks'), 
+    ],
+    [
+        State('uname', 'value'),
+        State('pword', 'value'), 
+        State('auth', 'data'),  # Retrieve auth data
+    ]
 )
-def try_log(submit,uname,pword):
-    if submit>0: 
-                    print('This is triggered')
-                    sql="SELECT account_password FROM user_account WHERE user_name="
-                    if uname:
-                        sql+="'"+uname+"'"
-                    else:
-                        sql+='0'
-                    print(sql)
-                    values=[]
-                    cols=['pass']
-                    df = db.querydatafromdatabase(sql, values, cols)
-                    if df.shape[0]:
-                        if (df['pass'][0]==pword):
-                            print('authenticated')
-                            return dash.no_update,{'isAuthenticated':True,'acc':uname}
-                        else:
-                            return ["Account or Password mismatch"],dash.no_update
-    raise PreventUpdate
+def loginprocess(loginbtn, username, password, auth_data):    
+    ctx = callback_context
+    if ctx.triggered:
+        alert_open = False 
+        alert_color = ""
+        alert_text = ""
+
+        eventid = ctx.triggered[0]['prop_id'].split('.')[0] 
+        
+        if eventid == 'login_loginbtn':
+            if loginbtn and username and password:
+                
+                sql = """
+                SELECT  account_password
+                FROM user_account
+                WHERE
+                    user_name = %s 
+                """
+                            
+                encrypt_string = lambda string: hashlib.sha256(string.encode('utf-8')).hexdigest()
+                values = [username]
+                cols = ['account_password']
+
+                df = db.querydatafromdatabase(sql, values, cols)
+                if df.shape[0]:
+                    auth_data['isAuthenticated'] = True  # Set isAuthenticated to True upon successful login
+                    alert_color = 'success'
+                    alert_text = 'Successfully logged in.'
+                    alert_open = True
+                else:
+                    alert_color = 'danger'
+                    alert_text = 'Incorrect username or password.'
+                    alert_open = True
+            
+        return [alert_color, alert_text, alert_open, auth_data]
+    else:
+        raise PreventUpdate
+
+
+@app.callback(
+    Output('pword', 'type'),
+    [Input('show_pword', 'value')]
+)
+def toggle_password_visibility(checked_values):
+    if checked_values:
+        return 'text'
+    else:
+        return 'password'
